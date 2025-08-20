4d89d4e4650b14e874fcbaf96d4bb81f9f01346f
Avinash kumar <mail2avinashkumar1@.com>
2025-08-20 13:54:32 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
       <img
         className="w-44 "
         src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
         alt="header logo"
       />
-      
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,21 +2,56 @@ import React from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = React.useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
 
-      <div>
+      <div className="absolute inset-0 -z-10 ">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_small.jpg"
           alt="image"
+          className="w-full h-full object-cover"
         />
       </div>
 
-      <form action="" className="bg-black">
-        <input type="text" placeholder="email" className="p-2 m-2" />
-        <input type="text" placeholder="password" className="p-2 m-2" />
-        <button>Sign In</button>
+      <form
+        action=""
+        className="bg-black/80 rounded-lg absolute  text-white p-8  my-36 mx-auto xl:w-4/12  lg:w-3/12   md:w-1/12 sm:w-1/12 left-0 right-0   "
+      >
+        <h1 className="font-bold text-3xl p-3 m-3">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Name"
+            className="p-3 mx-auto  my-3 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="email"
+          className="p-3 mx-auto  my-3 w-full bg-gray-700"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="p-3 mx-auto w-full my-3 bg-gray-700"
+        />
+        <button className="bg-red-900 w-full my-3 p-3 mx-auto rounded-lg ">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="p-6 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to netflix ? Sign up now"
+            : "Already have an account ? Sign in"}
+        </p>
       </form>
     </div>
   );
